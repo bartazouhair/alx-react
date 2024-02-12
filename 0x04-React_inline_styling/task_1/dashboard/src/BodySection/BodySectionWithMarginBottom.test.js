4e1849847edc3ee69c7fa9359f07909a46a194fd
Barta Zouhair <62765274+bartazouhair@users.noreply.github.com>
2024-02-12 19:10:48 +0100

    Update BodySectionWithMarginBottom.test.js

--- 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import BodySectionWithMarginBottom from "./BodySectionWithMarginBottom";
 import BodySection from "./BodySection";
-import { StyleSheetTestUtils } from "aphrodite";
+import { StyleSheetTestUtils } from 'aphrodite';
 
 beforeEach(() => {
   StyleSheetTestUtils.suppressStyleInjection();
